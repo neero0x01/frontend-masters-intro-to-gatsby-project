450f6b7d7164cd9d605c6ac05b0178102f3b1650
Muhammad Ahmad <181370123@gift.edu.pk>
2021-10-11 07:15:36 +0500

    link index to about page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,13 @@
+import { Link } from 'gatsby'
 import React from 'react'
 
 export default function IndexPage() {
   return (
     <main>
       <h1>Hello Gatsby</h1>
+      <Link to="/about">
+        About us
+      </Link>
     </main>
   )
 }
