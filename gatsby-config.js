96a2641dbf62e59e44dac91273a3a6d17da62ad8
Muhammad Ahmad <181370123@gift.edu.pk>
2021-10-11 07:29:07 +0500

    add gatsby config for metadata

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: 'https://www.domain.tld',
+    title: 'My Site',
+    description: 'Gatsby is awesome!',
+    image: 'https://unsplash.com/photos/dYEuFB8KQJk/download?force=true&w=640'
+  }
+}
