9e8ba769e0fa09ac4cd387b110cee4299afb1c84
Muhammad Ahmad <181370123@gift.edu.pk>
2021-10-11 07:52:32 +0500

    fetch and display title using graphql

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,28 @@
-import { Link } from 'gatsby'
+import { Link, useStaticQuery, graphql } from 'gatsby'
 import React from 'react'
 
 export default function IndexPage() {
+  const data = useStaticQuery(graphql`
+  query getSiteTitle {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+  `)
+  const meta = data?.site?.siteMetadata
   return (
-    <main>
-      <h1>Hello Gatsby</h1>
-      <Link to="/about">
-        About us
-      </Link>
-    </main>
+    <>
+      <header>
+        <h1>{meta?.title}</h1>
+      </header>
+      <main>
+        <h1>Hello Gatsby</h1>
+        <Link to="/about">
+          About us
+        </Link>
+      </main>
+    </>
   )
 }
